9e49fd5424e84a74a486e271f65201ebd3ebaeb4
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-06 09:41:32 -0300

    Script to git commands

--- git_commands.R
+++ git_commands.R
@@ -4,11 +4,11 @@ library(gert)
 
 # Adding files ----
 
-gert::git_add(list.files(pattern = "commands")) |> as.data.frame()
+gert::git_add(list.files(pattern = ".wav")) |> as.data.frame()
 
 # Commit file ----
 
-gert::git_commit("Script git commands")
+gert::git_commit("references .bib files")
 
 # Push ----
 
@@ -17,3 +17,7 @@ gert::git_push(remote = "origin", force = TRUE)
 # Pulll ----
 
 gert::git_pull()
+
+# Aborting commit ----
+
+gert::git_reset_hard(ref = "HEAD")
