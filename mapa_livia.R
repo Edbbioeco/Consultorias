dc64ae815bc814fb44d071013233680b4920c48c
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-12 16:52:06 -0300

    Script para criar os mapas de Livia

---
+++ mapa_livia.R
@@ -0,0 +1,21 @@
+# Pacotes ----
+
+library(readxl)
+
+library(tidyverse)
+
+library(geobr)
+
+library(sf)
+
+library(patchwork)
+
+# Dados ----
+
+## Coordenada ----
+
+### Importando ----
+
+livia <- readxl::read_xlsx("dados_livia.xlsx")
+
+### Visualizando ----
