d02fcb2ee0af373a8f69ec9197fa917efd9ad605
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-02 09:39:25 -0300

    Corrigido o nome do pacote adehabitatHR

--- mapas_wagner.R
+++ mapas_wagner.R
@@ -3,12 +3,12 @@
 if(require(c("tidyverse",
              "rnaturalearth",
              "geobr",
-             "adehabitatR")) == FALSE){ 
+             "adehabitatHR")) == FALSE){ 
   
   install.packages(c("tidyverse",
                      "rnaturalearth",
                      "geobr",
-                     "adehabitatR"))
+                     "adehabitatHR"))
   
 }
 
